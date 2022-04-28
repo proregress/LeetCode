6e037bdde2b013785293878551d3558508ebfe9e
proregress <37496223+proregress@users.noreply.github.com>
2022-04-28 17:50:50 +0800

    字符串、减法
    https://www.nowcoder.com/practice/a35ce98431874e3a820dbe4b2d0508b1?tpId=37&tqId=21225&rp=1&ru=/exam/oj/ta&qru=/exam/oj/ta&sourceUrl=%2Fexam%2Foj%2Fta%3Fdifficulty%3D2%26page%3D1%26pageSize%3D50%26search%3D%26tpId%3D37%26type%3D37&difficulty=2&judgeStatus=undefined&tags=&title=

---
+++ HJ2-计算某字符出现次数/Main.java
@@ -0,0 +1,30 @@
+/***
+描述:
+写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次数。（不区分大小写字母）
+
+数据范围： 
+1≤n≤1000
+
+输入描述：
+第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字符。
+输出描述：
+输出输入字符串中含有该字符的个数。（不区分大小写字母）
+***/
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        // 接受第一次输入的长字符串,并转换为小写
+        String str1 = sc.nextLine().toLowerCase();
+        // 接受单个字符，并转换为小写
+        String str2 = sc.nextLine().toLowerCase();
+        
+        int length1 = str1.length();
+        int length2 = str1.replaceAll(str2,"").length();
+        
+        System.out.println(length1 - length2);
+    }
+}
