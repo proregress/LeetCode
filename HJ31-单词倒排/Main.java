1899c69c7c5fe875490de4614228428d326d9cfe
proregress <37496223+proregress@users.noreply.github.com>
2022-05-05 16:12:00 +0800

    Update Main.java

--- HJ31-单词倒排/Main.java
+++ HJ31-单词倒排/Main.java
@@ -1,10 +1,15 @@
 /***  2/26
+一、思路
 1.将非大小写字母的char全部替换成空格
     注意：只能用replace() 不能用replaceAll()
 2.用split(" ")将string分隔进String[]
 3.倒序输出
 
-一些比较：
+二、匹配非字母的字符进行分割
+1.我的思路：将非字母字符全部替换成空格，再用空格分隔
+2.其他：正则表达式[^a-zA-Z]分割：String[] words = str.split("[^A-Za-z]");
+
+三、一些比较：
 str = str.replace(String.valueOf(c)," ")时，运行时间:134ms，占用内存:17400KB
 改为str = str.replace(c,' ')后               运行时间:48ms，占用内存:11548KB，提升30%多
 ***/
@@ -26,5 +31,6 @@ public class Main {
                 System.out.print(strArr[i] + " ");
             }     
         }
+        
     }
 }
