b9ed7cb94d8885a5430fa49d36334608261b72bc
proregress <37496223+proregress@users.noreply.github.com>
2022-05-06 16:43:25 +0800

    Create Main.java

---
+++ HJ97-记负均正/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        while (in.hasNext()) { 
+            int n = Integer.valueOf(in.nextLine());
+            String str = in.nextLine();
+            String[] chs = str.split(" ");
+            int countNeg = 0; //负数的个数
+            int countPos = 0; //正数的个数
+            int sum = 0;      //正数的和
+            for(String c : chs){
+                if(Integer.valueOf(c) < 0){
+                    countNeg = countNeg + 1;
+                }else if(Integer.valueOf(c) > 0){
+                    sum = sum + Integer.valueOf(c);
+                    countPos = countPos + 1;
+                }
+            }
+            if(countPos == 0){
+                System.out.println(countNeg + " "+ 0.0);
+            }else{
+                System.out.println(countNeg + " "+ String.format("%.1f", sum * 1.0/ countPos));
+            }
+        }
+           
+    }
+}
