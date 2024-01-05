e8e4d2c6db8124d48bfd7aec4ad13900235bbe3d
xufan <xufan_x@icloud.com>
2024-01-05 18:03:31 +0800

    117_填充每个节点的下一个右侧节点指针2【没懂，用层序遍历和116有啥不一样,同样的代码两题都过了】

--- src/二叉树/L0117_填充每个节点的下一个右侧节点指针2/Solution.java
+++ src/二叉树/L0117_填充每个节点的下一个右侧节点指针2/Solution.java
@@ -1,4 +1,4 @@
-package 二叉树.L0116_填充每个节点的下一个右侧节点指针;
+package 二叉树.L0117_填充每个节点的下一个右侧节点指针2;
 
 import 二叉树.entity.Node;
 
@@ -6,55 +6,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
-       1
+        1
    2       3
- 4  5    6  7
+ 4  5       7
  */
-class Solution {
+/**
+ * 层序遍历
+ */
+public class Solution {
     public static void main(String[] args) {
         Node root = new Node();
         Node left = new Node();
         Node leftLeft = new Node();
         Node leftRight = new Node();
         Node right = new Node();
-        Node rightLeft = new Node();
         Node rightRight = new Node();
         root.val = 1;
         left.val = 2;
         right.val = 3;
         leftLeft.val = 4;
         leftRight.val = 5;
-        rightLeft.val = 6;
         rightRight.val = 7;
         root.left = left;
         root.right = right;
         left.left = leftLeft;
         left.right = leftRight;
-        right.left = rightLeft;
         right.right = rightRight;
 
         Node connect = connect(root);
         System.out.println("connect = " + connect);
     }
+
     public static Node connect(Node root) {
-        //二叉树的层序遍历
-        if (root == null) {
+        if(root == null) {
             return null;
         }
+
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            for(int i = 0 ; i < size; i++){
                 Node poll = queue.poll();
-                if (poll.left != null) {
+                if(poll.left != null){
                     queue.offer(poll.left);
                 }
-                if (poll.right != null) {
+                if(poll.right != null){
                     queue.offer(poll.right);
                 }
-
-                if (i < size - 1) {
+                if(i < size-1){
                     poll.next = queue.peek();
                 }
             }
