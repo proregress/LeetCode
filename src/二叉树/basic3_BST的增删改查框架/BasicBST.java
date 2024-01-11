f79e62516a3621ca784da3f14371cbed54c0fa56
xufan <xufan_x@icloud.com>
2024-01-11 16:45:50 +0800

    L0450_删除二叉搜索树中的节点: 三种情况，仔细研究，详见BasicBST。

--- src/二叉树/basic3_BST的增删改查框架/BasicBST.java
+++ src/二叉树/basic3_BST的增删改查框架/BasicBST.java
@@ -2,7 +2,7 @@ package 二叉树.basic3_BST的增删改查框架;
 
 import 二叉树.entity.TreeNode;
 
-public class BasicIsInTree {
+public class BasicBST {
     /**
      * 判断某元素是否在一棵二叉树内
      */
@@ -63,8 +63,23 @@ public class BasicIsInTree {
      * 在BST中删除一个数的框架
      */
     TreeNode deleteNode(TreeNode root, int key){
+        if(root == null) return null;
         if(root.val == key){
             //找到，进行删除
+            //情况1：root没有子节点
+            if(root.left == null && root.right == null) return null;
+            //情况2：root只有一个子节点
+            if(root.left == null )return root.right;
+            if(root.right == null )return root.left;
+            //情况3：root两个子节点都要，那么：找到左子树中最大的那个节点，或者右子树中最小的那个节点来接替自己。此处以后者为例
+//            if(root.left != null && root.right != null){
+            //找到右子树中最小的节点
+            TreeNode minNode = getMin(root.right);
+            // 把 root 改成 minNode
+            root.val = minNode.val;
+            // 转而去删除 minNode
+            root.right = deleteNode(root.right, minNode.val);
+//            }
         }else if(root.val > key){
             //去左子树找
             root.left = deleteNode(root.left, key);
@@ -73,6 +88,17 @@ public class BasicIsInTree {
             root.right = deleteNode(root.right, key);
         }
         return root;
+    }
+
+    /**
+     * 得到二叉搜索树中的最小节点
+     * @param node
+     * @return
+     */
+    TreeNode getMin(TreeNode node){
+        //BST 最左边的就是最小的
+        while(node.left != null) node = node.left;
+        return node;
     }
 
 }
